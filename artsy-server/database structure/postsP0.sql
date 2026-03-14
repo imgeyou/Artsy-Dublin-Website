7f0cae3f8013eaf1452f3d4dfe653582c2fa2084
Ge You <youge071@gmail.com>
2026-03-14 11:24:29 +0000

    Reorganize and redefine post-related tables

--- artsy-server/database structure/postsP0.sql
+++ artsy-server/database structure/postsP0.sql
@@ -1,4 +1,23 @@
 -- all the tables storing data regrading post/comment, log-event, like
+CREATE TABLE `posttype` (
+  `typeId` int NOT NULL,
+  `typeName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`typeId`)
+);
+
+CREATE TABLE `eventattended` (
+  `eventAttendId` int NOT NULL AUTO_INCREMENT,
+  `userId` int DEFAULT NULL,
+  `eventId` int DEFAULT NULL,
+  `attendedAt` timestamp NULL DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT (now()),
+  `rating` int DEFAULT NULL,
+  PRIMARY KEY (`eventAttendId`),
+  KEY `userId` (`userId`),
+  KEY `eventId` (`eventId`),
+  CONSTRAINT `eventattended_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`),
+  CONSTRAINT `eventattended_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `events` (`eventId`)
+);
 
 CREATE TABLE `posts` (
   `postId` int NOT NULL AUTO_INCREMENT,
@@ -24,8 +43,7 @@ CREATE TABLE `posts` (
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`),
   CONSTRAINT `posts_ibfk_4` FOREIGN KEY (`postParentId`) REFERENCES `posts` (`postId`),
   CONSTRAINT `posts_ibfk_5` FOREIGN KEY (`eventAttendId`) REFERENCES `eventattended` (`eventAttendId`)
-) 
-
+);
 
 CREATE TABLE `postimages` (
   `postImageId` int NOT NULL AUTO_INCREMENT,
@@ -35,7 +53,6 @@ CREATE TABLE `postimages` (
   KEY `postId` (`postId`),
   CONSTRAINT `postimages_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`)
 ) 
-
 
 CREATE TABLE `postlikes` (
   `likeId` int NOT NULL AUTO_INCREMENT,
@@ -47,24 +64,4 @@ CREATE TABLE `postlikes` (
   KEY `postId` (`postId`),
   CONSTRAINT `postlikes_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`),
   CONSTRAINT `postlikes_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`)
-) 
-
-CREATE TABLE `posttype` (
-  `typeId` int NOT NULL,
-  `typeName` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`typeId`)
-) 
-
-CREATE TABLE `eventattended` (
-  `eventAttendId` int NOT NULL AUTO_INCREMENT,
-  `userId` int DEFAULT NULL,
-  `eventId` int DEFAULT NULL,
-  `attendedAt` timestamp NULL DEFAULT NULL,
-  `createdAt` timestamp NULL DEFAULT (now()),
-  `rating` int DEFAULT NULL,
-  PRIMARY KEY (`eventAttendId`),
-  KEY `userId` (`userId`),
-  KEY `eventId` (`eventId`),
-  CONSTRAINT `eventattended_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`),
-  CONSTRAINT `eventattended_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `events` (`eventId`)
-) 
+);
