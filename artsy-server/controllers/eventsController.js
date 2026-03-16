a9caa3f7ef481cdc619794019196c1b5ad6361be
Ge You <youge071@gmail.com>
2026-03-16 02:35:18 +0000

    add simple getEventById method for testing

--- artsy-server/controllers/eventsController.js
+++ artsy-server/controllers/eventsController.js
@@ -1,6 +1,8 @@
 // this is the controller for event related stuff
+// TODO: const userId = req.session?.userId
 
 const model = require("../models/events");
+const postsModel = require("../models/posts");
 
 // these also have to be async functions because we need to await the return from mysql!
 // which in turn awaits the call from the api (model side)
@@ -19,7 +21,21 @@ async function update (req, res) {
     res.json(results);
 }
 
+async function getEventById (req, res) {
+    const eventDetail = await model.getEventById(req.params.eventid);
+    if(!eventDetail) return res.status(404).send('Event not found');
+
+    const userId = 1; // TODO: const userId = req.session?.userId
+    const attendance = userId
+        ? await postsModel.getAttendanceStatus(userId, req.params.eventid)
+        : null;
+
+    res.json({ ...eventDetail, attendance }); // attendance: null if not logged in / not attended, otherwise { eventAttendId, rating }
+}
+
+
 module.exports = {
     get,
-    update
+    update,
+    getEventById
 };
