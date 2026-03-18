eea87cbd547d64b7a5d700f4fbb5ba2592652ba0
astridasi <astrid1108@163.com>
2026-03-18 17:14:25 +0000

    Add files via upload

--- artsy-server/utils/dbconfig.js
+++ artsy-server/utils/dbconfig.js
@@ -1,10 +1,10 @@
 //This is the configure file for setting up database connection
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    //***the naming now is different in events and users model, need to make them consistent
-}
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  //***the naming now is different in events and users model, need to make them consistent
+};
