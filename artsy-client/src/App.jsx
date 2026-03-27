249c5a07b2538a57af4badd6b473cbadcd310b8a
Ge You <youge071@gmail.com>
2026-03-27 14:30:05 +0000

    ud

--- artsy-client/src/App.jsx
+++ artsy-client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import EventDetailPage from './pages/eventDetailPage'
+import EventDetailPage from './pages/postDetailPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/events/:eventId" element={<EventDetailPage />} />
+        <Route path="/posts/:postId" element={<PostDetailPage />} />
       </Routes>
     </BrowserRouter>
   )
